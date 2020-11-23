4d69055fe53a0af6d515fc479d6e3da2d1c50d70
smlnx <alexander.is.dev@gmail.com>
2020-11-23 23:49:33 +0300

    Refactor(src/) index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './assets/scss/main.scss';
 
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/app.component.jsx";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
