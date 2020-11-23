c779923e1f03910b9f73e6e24c869a7440142f48
smlnx <alexander.is.dev@gmail.com>
2020-11-23 23:41:09 +0300

    Add(src/components/) app.component.jsx

---
+++ src/components/app.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default () => (
+  <div class="container">
+    <main class="wrapper">
+      <form class="converter" action="">
+        <div class="convrter__currency">
+          <input class="converter__currency__input" type="number" />
+          <select class="converter__currency__select" name="" id="">
+            <option value="">USD</option>
+            <option value="">RUB</option>
+            <option value="" selected>EUR</option>
+          </select>
+        </div>
+        <i class="fas fa-exchange-alt"></i>
+        <div class="converter__currency">
+          <input class="converter__currency__input" type="number" />
+          <select class="converter__currency__select" name="" id="">
+            <option value="">USD</option>
+            <option value="">RUB</option>
+            <option value="">EUR</option>
+          </select>
+        </div>
+      </form>
+    </main>
+  </div>
+);
