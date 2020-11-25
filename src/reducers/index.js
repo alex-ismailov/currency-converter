8b189b22a6524f9e0ce769345838302131d5ce07
smlnx <alexander.is.dev@gmail.com>
2020-11-26 01:50:04 +0300

    Add(src/reducers/) index.js - scratch of state

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+/* *** state scratch: *** */
+// state = {
+//   currency: {
+//     base: 'RUB',
+//     rates: {
+//       RUB: 1,
+//       EUR: 0.0111095928,
+//       USD: 0.0132093058,
+//     },
+//     new: 'USD',
+//   },
+//   form: {
+//     baseValue: 0,
+//     newValue: 0,
+//   },
+//   state: {
+//     process: 'filling', // processing
+//     isDisabledBtn: false,
+//   },
+// };
+// Возможно state.state стоит распределить по state.form
+
