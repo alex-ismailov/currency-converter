f962f1418106d12ff54aef4437be4aede44ff3f3
smlnx <alexander.is.dev@gmail.com>
2020-11-26 19:06:13 +0300

    Implement(src/reducers/index.js) reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -17,3 +17,53 @@
 // };
 // Возможно state.state стоит распределить по state.form
 
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import * as actions from '../actions/index.js';
+
+const {
+  changeBaseCurrency,
+  changeNewCurrency,
+  updateBaseCurrencyValue,
+  updateNewCurrencyValue,
+  updateProcessState,
+} = actions;
+
+const defaultState = {
+  currency: {
+    base: '',
+    rates: {},
+    new: '',
+  },
+  form: {
+    baseValue: 0,
+    newValue: 0,
+    processState: '',
+  },
+};
+
+const currency = handleActions({
+    [changeBaseCurrency]: (state, { payload: { baseCurrency }}) => {
+      return { ...state, base: baseCurrency };
+    },
+    [changeNewCurrency]: (state, { payload: { newCurrency }}) => {
+      return { ...state, new: newCurrency };
+    },
+}, defaultState.currency);
+
+const form = handleActions({
+  [updateBaseCurrencyValue]: (state, { payload: { baseValue }}) => {
+    return { ...state, baseValue };
+  },
+  [updateNewCurrencyValue]: (state, { payload: { newValue }}) => {
+    return { ...state, newValue };
+  },
+  [updateProcessState]: (state, { payload: { processState }}) => {
+    return { ...state, processState };
+  },
+}, defaultState.form);
+
+export default combineReducers({
+  currency,
+  form,
+});
