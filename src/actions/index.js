84ae11c842a2399cbb66e45d8ff6388ed271456d
smlnx <alexander.is.dev@gmail.com>
2020-12-02 00:42:52 +0300

    Add(src/actions/index.js) updateLeftSelect reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,4 +3,5 @@ import { createAction } from 'redux-actions';
 export const updateBaseCurrency = createAction('BASE_CURRENCY_UPDATE');
 export const updateLeftInput = createAction('LEFT_INPUT_VALUE_UPDATE');
 export const updateRightInput = createAction('RIGHT_INPUT_VALUE_UPDATE');
+export const updateLeftSelect = createAction('LEFT_SELECT_CURRENCY_UPDATE');
 // export const updateProcessState = createAction('PROCESS_STATE_UPDATE');
