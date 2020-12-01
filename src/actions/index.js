16f50e41f632c2813af46a73fa2f5bf93f81dda6
smlnx <alexander.is.dev@gmail.com>
2020-12-02 00:43:16 +0300

    Add(src/actions/index.js) updateRightSelect reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,4 +4,5 @@ export const updateBaseCurrency = createAction('BASE_CURRENCY_UPDATE');
 export const updateLeftInput = createAction('LEFT_INPUT_VALUE_UPDATE');
 export const updateRightInput = createAction('RIGHT_INPUT_VALUE_UPDATE');
 export const updateLeftSelect = createAction('LEFT_SELECT_CURRENCY_UPDATE');
+export const updateRightSelect = createAction('RIGHT_SELECT_CURRENCY_UPDATE');
 // export const updateProcessState = createAction('PROCESS_STATE_UPDATE');
