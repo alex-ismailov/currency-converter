d48db9bd34de929c6dfc813b56387e9ab015850a
smlnx <alexander.is.dev@gmail.com>
2020-12-01 16:32:24 +0300

    Refactor(src/components/CurrencyItem.jsx) props for the new state

--- src/components/CurrencyItem.jsx
+++ src/components/CurrencyItem.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 
 export default (props) => {
   const {
-    currency, value, handleInput, currenciesKeys,
+    value,
+    currency,
+    currenciesKeys,
+    handleInput,
+    selectId,
+    inputId,
   } = props;
 
   return (
     <div className="converter__currency">
-      <input onChange={handleInput} className="converter__currency__input" type="number" value={value} />
-      <select value={currency.toLowerCase()} className="converter__currency__select" name="" id="">
-        {currenciesKeys.map((key) => <option key={key} value={key.toLowerCase()}>{key}</option>)}
+      <input
+        id={inputId}
+        onChange={handleInput}
+        className="converter__currency__input"
+        type="number" value={value}
+        data-select-id={selectId}
+      />
+      <select
+        id={selectId}
+        value={currency}
+        className="converter__currency__select"
+      >
+        {currenciesKeys.map((key) => <option key={key} value={key}>{key}</option>)}
       </select>
     </div>
   );
