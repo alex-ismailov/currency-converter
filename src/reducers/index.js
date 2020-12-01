d1e1c3b71d74402e9b3fdec08a04f03b69c0a7d3
smlnx <alexander.is.dev@gmail.com>
2020-12-01 16:40:28 +0300

    Clear(src/reducers/index.js) remove comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,32 +1,3 @@
-/* *** state scratch: *** */
-// state = {
-//   currency: {
-//     base: 'RUB',
-//     rates: {
-//       RUB: 1,
-//       EUR: 0.0111095928,
-//       USD: 0.0132093058,
-//     },
-//     new: 'USD',
-//   },
-//   form: {
-//     baseValue: 0,
-//     newValue: 0,
-//     processState: 'filing',
-//   },
-// };
-// Возможно state.state стоит распределить по state.form
-
-// Пока идея такая: При смене state.currency.base запрашиваем новую data:
-// axios.get('https://api.exchangeratesapi.io/latest?base=# new state.currency.base #');
-
-// TODO !!!: Кэширование, использ.подсказки от Александр Усков из слак:
-// - можно использовать внутренние хранилища браузера (indexedDB) и хранить дату последнего обновления.
-//   обновлять при истечении срока давности, кеширование обычно делается так.
-// - можно использовать localStorage, смотря сколько данны надо хранить.
-//   В принципе суть одна, и localStorage чуть больше поддерживается.
-// - у LocalStorage есть ограничение по размеру. Если вы в него вписываетесь, то его достаточно.
-
 import { combineReducers } from 'redux';
 import { handleActions } from 'redux-actions';
 import * as actions from '../actions/index.js';
