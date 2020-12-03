d7471dfff3ab0ec833998723a9620fc56ea28dbf
smlnx <alexander.is.dev@gmail.com>
2020-12-04 01:15:20 +0300

    Add(src/actions/index.js) updateProcessState action

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,4 +5,4 @@ export const updateLeftInput = createAction('LEFT_INPUT_VALUE_UPDATE');
 export const updateRightInput = createAction('RIGHT_INPUT_VALUE_UPDATE');
 export const updateLeftSelect = createAction('LEFT_SELECT_CURRENCY_UPDATE');
 export const updateRightSelect = createAction('RIGHT_SELECT_CURRENCY_UPDATE');
-// export const updateProcessState = createAction('PROCESS_STATE_UPDATE');
+export const updateProcessState = createAction('PROCESS_STATE_UPDATE');
